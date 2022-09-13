9d6b75e86cc2afa4d2a26ae13509be3260f04c08
Dominick Cortez <jdcortez2268@gmail.com>
2022-09-13 08:39:36 -0700

    Create explore component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import Category from './components/Category'
 import Navbar from './components/Navbar'
 import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
@@ -17,6 +18,10 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Explore />} />
                     <Route path="/offers" element={<Offers />} />
+                    <Route
+                        path="/category/:categoryName"
+                        element={<Category />}
+                    />
                     <Route path="/profile" element={<PrivateRoute />}>
                         <Route path="/profile" element={<Profile />} />
                     </Route>
