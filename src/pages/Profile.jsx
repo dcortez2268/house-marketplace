7b5757c4ebb85033bc86d7bb8d3964d2265443a9
Dominick Cortez <jdcortez2268@gmail.com>
2022-09-13 12:02:53 -0700

    Add CreateListing component.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import { getAuth, updateProfile } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
     const navigate = useNavigate()
@@ -97,6 +99,12 @@ function Profile() {
                         />
                     </form>
                 </div>
+
+                <Link to="/create-listing" className="createListing">
+                    <img src={homeIcon} alt="home" />
+                    <p>Sell or rent your home</p>
+                    <img src={arrowRight} alt="right arrow" />
+                </Link>
             </main>
         </div>
     )
